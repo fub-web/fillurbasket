5d9f7409accd6e4c5a5a0e92236c49343ee282f3
Aman Aditya <aman.aditya@bluestacks.com>
2020-04-05 18:21:48 +0530

    adding lottie files for animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,26 @@
-import React, { Component } from "react";
-
+import React, { Component, Fragment } from "react";
+import Lottie from "react-lottie";
+import animationData from "./animationdata.json";
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <h2>Home </h2>;
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData
+      // rendererSettings: {
+      //   preserveAspectRatio: "xMidYMid slice"
+      // }
+    };
+    return (
+      <Fragment>
+        <h2>Home </h2>
+        <Lottie options={defaultOptions} height={400} width={400} />
+      </Fragment>
+    );
   }
 }
 
